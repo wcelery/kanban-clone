e192a4a0dab74a8b00a82531037e95b387fa220f
wcelery <horoschun.yura@yandex.ru>
2021-07-16 19:18:18 +0800

    helper route which makes use of custom hook

---
+++ src/utils/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "./useAuth";
+
+export function ProtectedRoute({ children, ...rest }) {
+  const { user } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
