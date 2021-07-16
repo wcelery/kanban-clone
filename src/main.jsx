3373858143ce758da5f11a197ab1180e623eb386
wcelery <horoschun.yura@yandex.ru>
2021-07-16 19:19:38 +0800

    wrap App with various providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.render(
   //eslint-disable-next-line
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
