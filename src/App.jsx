9113ab51a4c0ed8872cae525ef74dc3633cbd4c9
wcelery <horoschun.yura@yandex.ru>
2021-07-16 15:39:05 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Test from "./test";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className="App">
+      <Test />
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  //eslint-disable-next-line
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
